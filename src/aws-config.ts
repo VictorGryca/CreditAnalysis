5de020fd242d12336144f380ed287e4c8a6105d0
Victor Grycajuk <victorgryca@gmail.com>
2026-01-16 13:24:04 -0300

    fix: removes 'ssr' property

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -6,7 +6,5 @@ Amplify.configure({
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
     }
-  },
-  // Garante que a sessão persista mesmo após fechar o navegador
-  ssr: false,
+  }
 });
